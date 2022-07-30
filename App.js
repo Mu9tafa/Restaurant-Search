a50d60b0edba88a2ab7d3c4f866bd686e9e0142a
Mustafa <mu9tafa.805@gmail.com>
2022-07-30 12:19:55 +0200

    added SearchScreen and stack nivigation

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StatusBar } from 'react-native';
 
-export default function App() {
+import SearchScreen from './screens/SearchScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar backgroundColor={'black'} />
+      <Stack.Navigator
+        screenOptions={{
+          headerMode: 'screen',
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: 'tomato' },
+          headerTitleAlign: 'center',
+        }}>
+        <Stack.Screen
+          name='Search'
+          component={SearchScreen}
+          options={{
+            gestureEnabled: true,
+            title: 'Business Search',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
